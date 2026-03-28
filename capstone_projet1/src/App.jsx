b773b641f8829dd9243bfdec06fc00e6caf15482
Vilmeus Steward <vilmeussteward@gmail.com>
2026-03-28 10:42:53 -0400

    Modification App.jsx

--- capstone_projet1/src/App.jsx
+++ capstone_projet1/src/App.jsx
@@ -1,12 +1,38 @@
 import { useState } from 'react'
-import './App.css'
+import EmployeeForm from "./components/EmployeeForm"
+import EmployeeList from "./components/EmployeeList"
+import './components/Employee.css'
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-    </>
-  )
+export default function App() {
+  const [employees, setEmployees] = useState([
+    {
+      id:1,
+      name: "Jean Pierre",
+      position: "Developpeur",
+      department:"IT",
+      status: "Actif",
+    },
+    {
+      id:2,
+      name: "Marie Paul",
+      position: "RH",
+      department:"Ressources humaine",
+      status: "Inactif",
+    },
+  ]);
+
+  const addEmployee =(newEmployee)=> {
+    setEmployees([...employees, newEmployee]);
+  };
+
+  return(
+    <div>
+    <EmployeeForm addEmployee={addEmployee}/>
+
+    <EmployeeList employees={employees}/>
+    </div>
+  );
 }
-export default App
+
